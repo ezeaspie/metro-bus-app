332f92d085924006d94b69a6d3aaf80fffd4bfdb
Ezequiel Espinoza <ezequielnoza@gmail.com>
2018-05-07 12:50:15 -0400

    Basic functionality complete. Ready for production testing.

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ app.get("/", (req,res) => {
     })
         .then((response) => { return response.json() })
         .then((response) => {
-            res.render('main.ejs', {busData : response});
-            console.log(response);
+            res.render('main.ejs', {busData : [response]});
         });
 });
 
-app.post("/", (req,res) => {
+app.post("/search", (req,res) => {
     let latitude = req.body.lat;
     let longitude = req.body.lon;
     console.log(latitude, longitude);
@@ -34,6 +33,58 @@ app.post("/", (req,res) => {
     })
         .then((response) => { return response.json() })
         .then((response) => {
+            res.render('search.ejs', {stopData : response});
+        });
+});
+
+app.post("/", (req, res) => {
+    let stopArray = JSON.parse(req.body.userStopArray);
+    let dataArray = [];
+    fetch(`https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=${stopArray[0]}`, {
+            method: "GET",
+            mode: 'cors',
+            headers: { 'api_key': 'f991b2d63e364fddbf62d9cbb9525417' },
+    })
+        .then((response) => { return response.json() })
+        .then((response) => {
+            dataArray.push(response);
+            if (stopArray[1] != undefined) {
+                fetch(`https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=${stopArray[1]}`, {
+                    method: "GET",
+                    mode: 'cors',
+                    headers: { 'api_key': 'f991b2d63e364fddbf62d9cbb9525417' },
+                })
+                .then((response) => { return response.json() })
+                .then((response) => {
+                    dataArray.push(response);
+                        res.render("home.ejs", { busData: dataArray });
+                        
+                });
+            }
+            else {
+                res.render("home.ejs", { busData: dataArray });
+            }
+        });
+});
+
+app.post("/usersearch", (req, res) => {
+    let searchTerm = req.body.searchTerm;
+
+    console.log(searchTerm);
+    fetch("https://api.wmata.com/Bus.svc/json/jStops", {
+        method: "GET",
+        mode: 'cors',
+        headers: { 'api_key': 'f991b2d63e364fddbf62d9cbb9525417' },
+    })
+        .then((response) => { return response.json() })
+        .then((response) => {
+            let searchResults = [];
+            for(let i = 0 ; i<response.Stops.length ; i++) {
+                if(response.Stops[i].Name.includes(searchTerm)){
+                    searchResults.push(response.Stops[i]);
+                }
+            }
+            res.render('search.ejs', { stopData: {Stops : searchResults} });
         });
 });
 
--- public/mainScript.js
+++ public/mainScript.js
@@ -1,5 +1,39 @@
 var x = document.getElementById("demo");
-console.log("WHO THE HECK IS HE");
+
+let userPreferences = [];
+let saveButtons = document.querySelectorAll(".save");
+
+
+localforage.getItem("userInfo").then(function(value) {
+    if (value != null) {
+        userPreferences = value;
+    }
+    if(value != null && document.title == "Next Bus Express - Search") {
+        for(let i=0 ; i<saveButtons.length ; i++) {
+            if(userPreferences.includes(saveButtons[i].id)) {
+                saveButtons[i].disabled = true;
+            }
+            if(userPreferences.length >= 2) {
+                for(let i = 0 ; i<saveButtons.length ; i++) {
+                    saveButtons[i].disabled = true;
+                }
+            }
+        }
+        return;
+    }
+    if (document.title == "Next Bus Express" && value != [] && value != null) {
+        document.getElementById("array").value = JSON.stringify(userPreferences);
+        document.getElementById("stop-data").submit();
+        return;
+    }
+    if (document.title == "Next Bus Express" && value == [] && value != null){
+        getLocation();
+        console.log("Getting stops near you...");
+        return;
+    }
+ 
+
+});
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -16,12 +50,92 @@ function sendPosition(position) {
     document.getElementById('coordinates').submit();
 }
 
-getLocation();
-
 document.querySelector(".search").addEventListener("click", () => {
     let inputs = document.querySelectorAll(".search-form input");
     for (let i=0 ; i<inputs.length ; i++) {
         inputs[i].classList.toggle("expand");
-        console.log('working');
+    }
+});
+
+document.querySelector(".locate").addEventListener("click", function() {
+    getLocation();
+});
+
+let removeButtons = document.querySelectorAll(".remove");
+
+for(let i=0 ; i<removeButtons.length ; i++) {
+    removeButtons[i].addEventListener("click", function() {
+        let index = Number(this.id);
+        userPreferences.splice(index,1);
+        document.querySelector(`#card${i}`).style.display = "none";
+        if(userPreferences.length == 0){
+            localforage.clear().then(function () {
+                console.log('Database is now empty.');
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+        else{
+            localforage.setItem('userInfo', userPreferences).then(function (value) {
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+        
+    });
+}
+
+for(let i = 0 ; i<saveButtons.length ; i++) {
+    saveButtons[i].addEventListener("click", function() {
+        userPreferences.push(this.id);
+        if (userPreferences.length >= 2) {
+            for (let i = 0; i < saveButtons.length; i++) {
+                saveButtons[i].disabled = true;
+            }
+        }
+        localforage.setItem('userInfo', userPreferences).then(function (value) {
+                console.log(value);
+        }).catch(function (err) {
+                console.log(err);
+        });
+
+        if (userPreferences.includes(this.id)) {
+            this.disabled = true;
+            return;
+        }
+    });
+}
+
+const countdown = (msTime) => {
+    let times = document.querySelectorAll(".time");
+    setInterval(() => {
+        for(let i = 0 ; i<times.length ; i++) {
+            let newTime = Number(times[i].innerHTML) -1;
+            if(newTime >= 0){
+                times[i].innerHTML = newTime;
+            }
+            else {
+                times[i].parentNode.innerHTML = "Bus has passed";
+            }
+        }
+    }, msTime);
+}
+countdown(60000);
+
+document.querySelector(".search-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let searchTerm = document.querySelector(".search-input").value;
+    searchTerm = searchTerm.toUpperCase();
+    if(searchTerm == "" || searchTerm == " ") {
+        document.querySelector(".search-input").classList.add("invalid");
+        return;
+    }
+    else{
+        let res = searchTerm.replace(/street/gi, "ST");
+        if(/avenue/gi.test(searchTerm)){
+            res = searchTerm.replace(/avenue/gi, "AVE");
+        }
+        document.querySelector(".search-input").value = res;
+       document.querySelector(".search-form").submit();
     }
 });
