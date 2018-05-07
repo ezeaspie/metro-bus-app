62784d8d38897e0808ddfef0f3558889a026c97e
Ezequiel Espinoza <ezequielnoza@gmail.com>
2018-05-07 13:11:01 -0400

    bug fixes

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ app.post("/usersearch", (req, res) => {
         });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server running.");
 });
